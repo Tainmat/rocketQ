bb4f0d24c9345c6b35f2fbe0c9e2106f70daaf9e
Tainã Martins Pino <63248706+Tainmat@users.noreply.github.com>
2022-05-30 07:45:26 -0300

    Montando servidor node

--- src/server.js
+++ src/server.js
@@ -1,2 +1,14 @@
 const express = require('express');
+const route = require('./routes.js');
 const port = '8080';
+const server = express();
+
+const path = require('path');
+server.set('view engine', 'ejs');
+server.set('views', path.join(__dirname, 'views'));
+
+server.use(route);
+
+server.listen(port, () => console.log(`http://localhost/${port}`));
+
+/* vídeo 4 min 47 */
